1b9c72d6a2b787b7fc5e3455cfdfe915ac80c53b
dan <danya.belyaev@gmail.com>
2024-03-21 17:30:49 +0300

    init proj

---
+++ src/test/java/org/test/AppTest.java
@@ -0,0 +1,12 @@
+package org.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void assertTrue() {
+        Assert.assertTrue(true);
+    }
+}
