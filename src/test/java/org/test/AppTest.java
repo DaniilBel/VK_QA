ebd3bcac60af1f43dcc2a0595ccc8e15602babb0
dan <danya.belyaev@gmail.com>
2024-03-28 15:25:57 +0300

    Partically

--- src/test/java/org/test/AppTest.java
+++ src/test/java/org/test/AppTest.java
@@ -1,12 +1,58 @@
 package org.test;
 
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.ElementsCollection;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.CollectionCondition.*;
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
 public class AppTest {
 
+    static String url = "";
+
+    @BeforeAll
+    public static void setUp() {
+        url = "https://google.com/ncr";
+    }
+
     @Test
-    public void assertTrue() {
-        Assert.assertTrue(true);
+    public void shouldSearch() {
+        open("https://google.com/ncr");
+
+        element(byName("q")).setValue("selenide").pressEnter();
+
+        ElementsCollection res = elements("#search .g");
+        res.shouldHave(sizeGreaterThanOrEqual(6))
+                .first().shouldHave(text("Selenide: concise UI tests in Java"));
+
+//        res.first().find(".r>a").click();
+//
+//        Wait().until(titleIs("Selenide: concise UI tests in Java"));
+    }
+
+    @Test
+    public void searchResultsContainSelenide() {
+        open("https://google.com/ncr");
+
+        element(byName("q")).setValue("selenide").pressEnter();
+
+        elements("#search .g").shouldHave(sizeGreaterThanOrEqual(0)).forEach(element ->
+                element.shouldHave(text("Selenide")));
+    }
+
+    @Test
+    public void searchResultsContainLinks() {
+        open("https://google.com/ncr");
+
+        element(byName("q")).setValue("selenide").pressEnter();
+
+        elements("#search .g").shouldHave(sizeGreaterThanOrEqual(0)).forEach(element ->
+                element.find("a").shouldHave(attribute("href")));
     }
 }
